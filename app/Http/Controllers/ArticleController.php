5269489b186b68af50aa55bfebc83f30b5d54594
Abel <kent62001@gmail.com>
2017-04-23 10:37:10 +0800

    [文章] - 新增Resource路徑、controller action

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class ArticleController extends Controller
+{
+    public function __construct()
+    {
+
+    }
+
+    public function index()
+    {
+        
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+}
--- routes/auth.php
+++ routes/auth.php
@@ -1,9 +1,24 @@
 <?php
 
 
+use App\Teach\User\Constant\PermissionConstant;
 use App\Teach\User\Constant\RoleConstant;
 
 Route::get('/home', 'HomeController@index');
+
+
+// https://laravel.com/docs/master/controllers#resource-controllers
+Route::get('/articles', 'ArticleController@index')->name('articles:index');
+Route::get('/articles/create', 'ArticleController@create')->name('articles:create');
+Route::post('/articles', 'ArticleController@store')->name('articles:store');
+Route::get('/articles/{article_id}', 'ArticleController@show')->name('articles:show');
+Route::get('/articles/{article_id}/edit', 'ArticleController@edit')->name('articles:edit');
+Route::put('/articles/{article_id}', 'ArticleController@update')->name('articles:update');
+Route::delete('/articles/{article_id}', 'ArticleController@destroy')->name('articles:destroy');
+//Route::resource('articles', 'ArticleController');
+
+
+
 
 
 Route::group(['middleware' => 'role:'. RoleConstant::ADMIN_ROLE_NAME], function (){
