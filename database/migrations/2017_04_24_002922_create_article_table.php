ed8ac97f505e25f04a3078227187d83852e9f92d
Abel <kent62001@gmail.com>
2017-04-24 08:35:38 +0800

    [文章] - 新增 migration

---
+++ database/migrations/2017_04_24_002922_create_article_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function(Blueprint $table){
+            $table->increments('id');
+            $table->string('title');
+            $table->string('content');
+            $table->string('status');
+            $table->timestamp('published_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
