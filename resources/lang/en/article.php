b5c838711150b696923e80f8c2eb3b921b5755cb
Abel <kent62001@gmail.com>
2017-05-07 20:34:42 +0800

    [多國語言] - 新增中文

---
+++ resources/lang/en/article.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    // form placeholder
+    'placeholder' => [
+        'title' => 'Please enter article title',
+        'published_at' => 'Please enter article publish time',
+        'content' => 'Please enter article content',
+    ]
+];
---
+++ resources/lang/zh-TW/article.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    // form placeholder
+    'placeholder' => [
+        'title' => '請輸入文章標題',
+        'published_at' => '請輸入文章發佈時間',
+        'content' => '請輸入文章內容',
+    ]
+];
